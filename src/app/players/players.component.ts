b5f1bca67de813493f70140d96f5bfd120ebe20f
nunoedgar <edgarpedra99@gmail.com>
2024-09-12 20:54:03 +0100

    Update player (CORS error)

--- src/app/players/players.component.ts
+++ src/app/players/players.component.ts
@@ -3,17 +3,27 @@ import { PlayerService } from '../services/player.service';
 import { Player } from '../Model/Player';
 import { CommonModule } from '@angular/common';
 import { TableModule } from 'primeng/table';
+import { ToastModule } from 'primeng/toast';
+import { TagModule } from 'primeng/tag';
+import { DropdownModule } from 'primeng/dropdown';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-players',
   standalone: true,
-  imports: [CommonModule, TableModule],
+  imports: [CommonModule, TableModule, ToastModule, TagModule, DropdownModule, ButtonModule, InputTextModule, FormsModule],
+  providers: [MessageService],
   templateUrl: './players.component.html',
   styleUrl: './players.component.css'
 })
 export class PlayersComponent implements OnInit {
 
   players : Player[] = [];
+
+  clonedPlayers: { [s: string]: Player } = {};
 
   constructor(private playerService : PlayerService){}
 
@@ -23,9 +33,26 @@ export class PlayersComponent implements OnInit {
 
   getAllPlayers() : void {
     this.playerService.getAllPlayers().subscribe(data =>{
-      console.log(data);
+      //console.log(data);
       this.players = data;
     })
+  }
+
+  onRowEditInit(player: Player) {
+    this.clonedPlayers[player.id as number] = { ...player };
+}
+
+  onRowEditSave(player: Player) {  
+    console.log("==MÉTODO  onRowEditSave==")  
+    this.playerService.updatePlayer(player).subscribe(res =>{
+      console.log("==UPDATED PLAYER==");
+      console.log(res);
+    })
+  }
+
+  onRowEditCancel(player: Player, index: number) {
+      this.players[index] = this.clonedPlayers[player.id as number];
+      delete this.clonedPlayers[player.id as number];
   }
 
 }
--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -1,17 +1,25 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Player } from '../Model/Player';
+import { environment } from '../../environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlayerService {
 
-  private apiUrl = 'http://localhost:8000/api/player';
+  private apiUrl = environment.apiUrl + '/player';
 
   constructor(private http: HttpClient) {}
 
   getAllPlayers(): Observable<any> {
     return this.http.get(this.apiUrl);
+  }
+
+  updatePlayer(player: Player): Observable<any> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const url = `${this.apiUrl}/${player.id}`;  
+    return this.http.put(url, player, {headers});
   }
 }
